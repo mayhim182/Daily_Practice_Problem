fd3aa99c14ffd23d55c5cd8c647dc99730b9b109
mayhim182 <mayankatnitrkl@gmail.com>
2023-11-26 20:04:38 +0530

    Daily code push

---
+++ qn2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countMinimumOperations(vector<int> &arr){
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+
+    for(auto &x:arr){
+        cin>>x;
+    }
+    return 0;
+}
